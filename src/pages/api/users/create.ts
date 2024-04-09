fa37db1fecef4fc20aa5913804ff1abbcfec8aad
Fabricio Carvalho <fcarvalho1999@gmail.com>
2024-04-09 11:44:27 -0300

    Integrate endpoint for creating users

--- src/pages/api/users/create.ts
+++ src/pages/api/users/create.ts
@@ -17,5 +17,19 @@ import { IUser, IUserCreate } from '@/types/user.d';
 const users: IUser[] = [];
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	return res.status(400).json(undefined);
+	if (req.method !== 'POST') return res.status(405).json({ message: 'Método não permitido' });
+
+	const { name, email }: IUserCreate = req.body;
+
+	if (!name || !email) return res.status(400).json({ message: 'Campos obrigatórios não preenchidos' });
+
+	if (users.find((user) => user.email === email)) return res.status(400).json({ message: 'E-mail já cadastrado' });
+
+	const id = users.length + 1;
+	const user: IUser = { id, name, email };
+
+	users.push(user);
+
+	return res.status(201).json(user);
+
 };
